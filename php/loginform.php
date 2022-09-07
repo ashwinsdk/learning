f744c7668b9462cd88ddc7a4696a193e5069f2f7
ashwin_sdk <ashwin2005s@gmail.com>
2022-09-07 18:11:18 +0530

    save changes

---
+++ php/loginform.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    <title>Social Login</title>
+    </head>
+
+    <body>
+        <form action="" method="POST">
+            <input type="text" name="user" placeholder="username">
+            <input type="text" name="password" placeholder="password">
+            <button type="submit" name="submit">Submit</button>
+        </form>
+    </body>
+</html>
+
