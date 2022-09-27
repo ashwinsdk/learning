0ea54595425552c5fb37acf7eb55cbb8d1bab101
ashwin_sdk <ashwin2005s@gmail.com>
2022-09-27 13:08:48 +0530

    save

--- sociallogin/index.php
+++ sociallogin/index.php
@@ -1,10 +1,17 @@
 <?php
 session_start();
 require "requiredata.php";
+
 $session=$_SESSION["email"];
 if($session==true){}
 else{
     header('location:login.php');
 }
-echo "hai"
+
 ?>
+<html>
+    <head><title></title></head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
--- sociallogin/login.php
+++ sociallogin/login.php
@@ -4,26 +4,45 @@
   <title>Login</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-  <form action="userlog.php" method="POST">
-    <div style="margin:300px;padding-left:280px;">
-      
-      <div class="mb-3 mt-3" style="width:300px;">
-        <label for="email" class="form-label">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-      </div>
-      
-      <div class="mb-3" style="width:300px;">
-        <label for="pwd" class="form-label">Password:</label>
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
-      </div>
-      
-      <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-    
+<div class="login">
+
+<ul class="pagination" style="margin: left 30px;">
+  <li class="page-item"><a class="page-link" href="login.php">Login</a></li>
+  <li class="page-item"><a class="page-link" href="register.php">Register</a></li>
+</ul>
+
+<form class="needs-validation" action="userlog.php" method="POST">
+    <div class="form-group was-validated">
+        <label class="form-label" for="email">Email address</label>
+        <input class="form-control" type="text" name="email" required>
+        <div class="invalid-feedback">
+            Please enter your email address
+        </div>
     </div>
-  </form>
+
+    <div class="form-group was-validated">
+        <label class="form-label" for="password">Password</label>
+        <input class="form-control" type="text" name="password" required>
+        <div class="invalid-feedback">
+            Please enter your password
+        </div>
+    </div>
+
+     <div class="form-group form-check">
+        <input class="form-check-input" type="checkbox" id="check">
+        <label class="form-check-label" for="check">Remember me</label>
+    </div>
+   
+   <input class="btn btn-success w-100" type="submit" name="submit" value="SIGN IN">
+
+</form>
+
+</div>
+
 </body>
 
 </html>
